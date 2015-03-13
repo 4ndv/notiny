8ec446ea88e3de15e99cbd8804dea51041cd9e7f
Andrey Viktorov <andreyviktorov@outlook.com>
2015-03-13 22:57:35 +0700

    Lots of things

--- README.md
+++ README.md
@@ -7,8 +7,18 @@ Ultra-small jquery notification plugin
 
 `$.notiny("Hello <b>world</b>!");`
 
+## Options
+
+* image - link to image, **default: undefined**
+* position - position on screen, **default: bottom right**
+* theme - theme name, **default: light**
+* width - width of notification, **default: 500px**
+* strip - strips long text to fit width, **default: true**
+
+## Themes
+
+Included themes: light, dark
+
 ## Demo
 
 You can see demo [here](http://andreyviktorov.github.io/notiny/)
-
-## Theming
--- src/notiny.js
+++ src/notiny.js
@@ -1,9 +1,85 @@
-(function( $ ) {
-  $.fn.notiny = function(text, options){
-    var settings = $.extend({
-      image: undefined,
-      position: "bottom right",
-      theme: "light"
-    }, options);
+(function($) {
+  var g_settings = {};
+
+  var defaults = {
+    image: undefined,
+    position: 'bottom right',
+    theme: 'light',
+    width: '300px',
+    strip: true
   };
-}( jQuery ));
+
+  var parsePosition = function() {
+    var newpos = [];
+    var x,y = false;
+    var args = g_settings.position.split(' ');
+
+    for(var i = 0; i<args.length; i++) {
+      if(!x) {
+        if(args[i] === 'left' || args[i] === 'right') {
+          x = true;
+          newpos.push(args[i]);
+          g_settings.float = args[i];
+        }
+      }
+
+      if(!y) {
+        if(args[i] === 'top' || args[i] === 'bottom') {
+          y = true;
+          newpos.push(args[i]);
+        }
+      }
+    }
+
+    if(!x) {
+      newpos.push('right');
+    }
+
+    if(!y) {
+      newpos.push('bottom');
+    }
+
+    g_settings.position = newpos.join(' ');
+  };
+
+  var createNotification = function(text, options) {
+    var elem = $('body');
+    g_settings = $.extend(defaults, options);
+    // Parse and verify position string
+    parsePosition();
+
+    // Local copy of settings
+    var settings = g_settings;
+
+    // Creating container
+    var containerId = 'notiny-container-' + settings.position_y + '-' + settings.position_x;
+    if ($('#' + containerId).length == 0) {
+      var container = $('<div/>', {
+        class: 'notiny-container',
+        id: containerId,
+        text: 'test'
+      });
+
+      container.css(settings.position_x, 0);
+      container.css(settings.position_y, 0);
+    }
+
+    elem.append(container);
+
+    // Creating notification
+    var notificationClass = 'notify-wrapper-' + settings.theme;
+    var notification = $('<div/>', {
+      class: notificationClass + ' notify-wrapper'
+    });
+  };
+
+  $.fn.notiny = function(text, options) {
+    createNotification(text, options);
+    return this;
+  };
+
+  $.notiny = function(text, options) {
+    createNotification(text, options);
+    return this;
+  }
+}(jQuery));
