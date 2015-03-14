c3e2baaa1deeeeb150b44f2947f3d0506f9eac71
Andrey Viktorov <andreyviktorov@outlook.com>
2015-03-15 02:39:39 +0700

    Animations and stuff

--- src/notiny.js
+++ src/notiny.js
@@ -10,11 +10,38 @@
     strip: true
   };
 
+  // https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Using_CSS_animations/Detecting_CSS_animation_support
+  var isAnimationSupported = function() {
+    var animation = false,
+      animationstring = 'animation',
+      keyframeprefix = '',
+      domPrefixes = 'Webkit Moz O ms Khtml'.split(' '),
+      pfx = '';
+
+    if (elm.style.animationName !== undefined) {
+      animation = true;
+    }
+
+    if (animation === false) {
+      for (var i = 0; i < domPrefixes.length; i++) {
+        if (elm.style[domPrefixes[i] + 'AnimationName'] !== undefined) {
+          pfx = domPrefixes[i];
+          animationstring = pfx + 'Animation';
+          keyframeprefix = '-' + pfx.toLowerCase() + '-';
+          animation = true;
+          break;
+        }
+      }
+    }
+
+    return animation;
+  };
+
   var checkPosition = function(g_settings) {
-    if(g_settings.x !== 'left' && g_settings.x !== 'right') {
+    if (g_settings.x !== 'left' && g_settings.x !== 'right') {
       g_settings.x = 'right';
     }
-    if(g_settings.y !== 'top' && g_settings.y !== 'bottom') {
+    if (g_settings.y !== 'top' && g_settings.y !== 'bottom') {
       g_settings.y = 'bottom';
     }
 
@@ -23,10 +50,9 @@
 
   var createNotification = function(text, options) {
     var elem = $('body');
-    console.log(defaults);
+
     var settings = $.extend({}, defaults);
     $.extend(settings, options);
-    console.log(defaults);
     // Parse and verify position string
     settings = checkPosition(settings);
     // Local copy of settings
@@ -48,25 +74,25 @@
     });
 
     // Strip
-    if(settings.strip) {
+    if (settings.strip) {
       ptext.css('white-space', 'nowrap');
       ptext.css('text-overflow', 'ellipsis');
     }
 
-    if(settings.image !== undefined && settings.background && !settings.strip) {
+    if (settings.image !== undefined && settings.background && !settings.strip) {
       ptext.css('padding-top', '0px');
     }
 
     // Image
-    if(settings.image !== undefined) {
-      var imgtd = $('<td/>');
+    if (settings.image !== undefined) {
+      var imgtd = $('<td/>', {
+        class: 'notiny-content-img'
+      });
 
       var img = $('<img/>', {
         src: settings.image,
         class: 'notiny-content-img'
       });
-
-      imgtd.css('width', img.width());
 
       ptext.css('padding-left', '6px');
 
@@ -104,11 +130,47 @@
     }
 
     wrapper.append(notification);
-
     container.prepend(wrapper);
-    wrapper.fadeIn(500);
 
-    setTimeout(function(){wrapper.fadeOut(500);}, settings.delay);
+    var closing = false;
+
+    var closeAction = function() {
+      if (!closing) {
+        closing = true;
+        if (isAnimationSupported) {
+          notification.css('animation', 'notiny-animation-hide 0.5s forwards');
+          setTimeout(function() {
+            wrapper.remove();
+          }, 550);
+        } else {
+          // Fallback for old browsers
+          wrapper.fadeOut(400, function() {
+            wrapper.remove();
+          });
+        }
+      }
+    };
+
+    var showAction = function() {
+      if (isAnimationSupported) {
+        notification.css('animation', 'notiny-animation-show 0.4s forwards');
+      } else {
+        // Fallback for old browsers
+        wrapper.fadeIn(500);
+      }
+    };
+
+    showAction();
+
+    notification.click(function() {
+      closeAction();
+      return false;
+    })
+
+    setTimeout(function() {
+      closeAction();
+      // + half second from show animation
+    }, settings.delay + 500);
   };
 
   $.fn.notiny = function(text, options) {
